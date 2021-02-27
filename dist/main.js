7b9ad0784631c0c2798c1d1092a9a1d991063f37
Ngugisenior <ngugijoseph992@gmail.com>
2021-02-27 16:36:54 +0100

    by tags

--- dist/main.js
+++ dist/main.js
@@ -30,7 +30,7 @@ const App = () =>{
         }
     ]; 
     
-    listJobs();
+    //listJobs();
 
     function listJobs(){
 
@@ -45,7 +45,7 @@ const App = () =>{
     }
 
 
-
+    //fetch Jobs by tags
     function getFetchJobs(x){
 
          (async function fetchJobsByTags(){
@@ -57,10 +57,12 @@ const App = () =>{
         })();
     }
 
-
+    //view jobs
     function dataView(jobs){
         var m = document.getElementsByClassName('container')[0];
         m.innerHTML = ''
+
+  
         for(let i = 1; i < jobs.length; i++){
                             
             if(jobs[i].company === "" && jobs[i].company_logo === "" || jobs[i].url === ""){
@@ -73,77 +75,8 @@ const App = () =>{
                 const rf = document.createElement('div');
                 rf.setAttribute('class','job_desc')
 
+                createElements(jobs[i], y, rf);
 
-                for(let j = 0; j < f.length; j++){
-
-                    
-                    if(f[j].class == 'company_logo'){
-                        var dv = document.createElement('div');
-                        dv.setAttribute('class','jobs_img');
-                        dv.setAttribute('loading','lazy');
-                        var x = document.createElement(f[j].name);
-                        x.setAttribute('class',f[j].class);
-                        x.setAttribute('src', jobs[i][f[j].class]);
-
-                        //click event
-                        x.addEventListener('click',function(event){
-                            event.preventDefault();
-                            dataViewByCompany(jobs[i].company);
-                        });
-
-                        dv.appendChild(x);
-
-                        y.appendChild(dv);
-                    }
-                    else if(f[j].class == 'url'){
-                        
-                        var x = document.createElement(f[j].name);
-                        var btn = document.createElement('BUTTON');
-                        var t = document.createTextNode("Apply");
-
-                        btn.setAttribute('class', 'apply');
-                        
-                       
-                        x.setAttribute('class',f[j].class);
-                        x.setAttribute('href', jobs[i][f[j].class]);
-                        x.setAttribute('target', '_blank'); 
-
-                        btn.appendChild(t);
-                        x.appendChild(btn);
-                        rf.appendChild(x);
-                    }
-                    else if(f[j].class == 'tags'){
-
-                        var l = document.createElement('ul')
-                        l.setAttribute('class', 'tags_list');
-
-                        for(let k in jobs[i][f[j].class]){
-                            var x = document.createElement(f[j].name);
-                            x.setAttribute('class',f[j].class);
-                            
-                            x.addEventListener('click', function(event){
-                                event.preventDefault();
-                                var url_2 = url + "tags="+(jobs[i][f[j].class][k]).replace(/\s/g,'%20')
-                                getFetchJobs(url_2);
-                            });
-                            var t = document.createTextNode(jobs[i][f[j].class][k]);
-                            x.appendChild(t);
-
-                            l.appendChild(x);
-                        }
-                        rf.appendChild(l);
-                    }
-                    
-                    else{
-                        var x = document.createElement(f[j].name);
-                        x.setAttribute('class',f[j].class);
-                        x.innerHTML = jobs[i][f[j].class];
-                        rf.appendChild(x)
-                       // y.appendChild(x);
-
-                    }
-                    
-                }
                 y.appendChild(rf);
                 m.appendChild(y)
 
@@ -151,8 +84,82 @@ const App = () =>{
         }
     }
 
+    // create html elements
+    function createElements(job, y, rf){
+
+        for(let j = 0; j < f.length; j++){
+
+            if(f[j].class == 'company_logo'){
+                var dv = document.createElement('div');
+                dv.setAttribute('class','jobs_img');
+                dv.setAttribute('loading','lazy');
+                var x = document.createElement(f[j].name);
+                x.setAttribute('class',f[j].class);
+                x.setAttribute('src', job[f[j].class]);
+
+                //click event
+                x.addEventListener('click',function(event){
+                    event.preventDefault();
+                    dataViewByCompany(job.company);
+                });
+
+                dv.appendChild(x);
+
+                y.appendChild(dv);
+            }
+            else if(f[j].class == 'url'){
+                
+                var x = document.createElement(f[j].name);
+                var btn = document.createElement('BUTTON');
+                var t = document.createTextNode("Apply");
+
+                btn.setAttribute('class', 'apply');
+                
+               
+                x.setAttribute('class',f[j].class);
+                x.setAttribute('href', job[f[j].class]);
+                x.setAttribute('target', '_blank'); 
+
+                btn.appendChild(t);
+                x.appendChild(btn);
+                rf.appendChild(x);
+            }
+            else if(f[j].class == 'tags'){
+
+                var l = document.createElement('ul')
+                l.setAttribute('class', 'tags_list');
+
+                for(let k in job[f[j].class]){
+                    var x = document.createElement(f[j].name);
+                    x.setAttribute('class',f[j].class);
+                    
+                    x.addEventListener('click', function(event){
+                        event.preventDefault();
+                        var url_2 = url + "tags="+(job[f[j].class][k]).replace(/\s/g,'%20')
+                        getFetchJobs(url_2);
+                    });
+                    var t = document.createTextNode(job[f[j].class][k]);
+                    x.appendChild(t);
+
+                    l.appendChild(x);
+                }
+                rf.appendChild(l);
+            }
+            
+            else{
+                var x = document.createElement(f[j].name);
+                x.setAttribute('class',f[j].class);
+                x.innerHTML = job[f[j].class];
+                rf.appendChild(x)
+               // y.appendChild(x);
+
+            }
+            
+        }
+    }
     
 
+    // view Jobs by Company Names
     function dataViewByCompany(str){
         var m = document.getElementsByClassName('container')[0];
         m.innerHTML = ''
@@ -173,91 +180,109 @@ const App = () =>{
 
 
                if(jobs[i].company === str){
-                   console.log(jobs[i]);
-                   var y = document.createElement('div');
-                   y.setAttribute('class','jobs');
                    const rf = document.createElement('div');
-                   rf.setAttribute('class','job_desc')
-                   for(let j = 0; j < f.length; j++){
-   
-                       
-                       if(f[j].class == 'company_logo'){
-                           var dv = document.createElement('div');
-                           dv.setAttribute('class','jobs_img');
-                           dv.setAttribute('loading','lazy');
-                           var x = document.createElement(f[j].name);
-                           x.setAttribute('class',f[j].class);
-                           x.setAttribute('src', jobs[i][f[j].class]);
-                           x.addEventListener('click',function(event){
-                               event.preventDefault();
-                               dataViewByCompany(jobs[i].company);
-                           })
-                           dv.appendChild(x);
-                           //rf.appendChild(x)
-                           y.appendChild(dv);
-                       }
-                       else if(f[j].class == 'url'){
-                           var x = document.createElement(f[j].name);
-                           var btn = document.createElement('BUTTON');
-                           btn.setAttribute('class', 'apply');
-                           var t = document.createTextNode("Apply");
-                           btn.appendChild(t);
-                           x.setAttribute('class',f[j].class);
-                           x.setAttribute('href', jobs[i][f[j].class]);
-                           x.setAttribute('target', '_blank');
-                           x.appendChild(btn);
-                           rf.appendChild(x);
-                       }
-                       else if(f[j].class == 'tags'){
-                           var l = document.createElement('ul')
-                           l.setAttribute('class', 'tags_list');
-                           for(let k in jobs[i][f[j].class]){
-                               //const li = document.createElement('a');
-                               //li.setAttribute('href', url + "tags="+jobs[i][f[j].class][k]);
-                               //li.setAttribute('target', '_blank');
-                               var x = document.createElement(f[j].name);
-                               x.setAttribute('class',f[j].class);
-                               
-                               x.addEventListener('click', function(event){
-                                   event.preventDefault();
-                                   var url_2 = url + "tags="+(jobs[i][f[j].class][k]).replace(/\s/g,'%20')
-                                   getFetchJobs(url_2);
-                               });
-                               var t = document.createTextNode(jobs[i][f[j].class][k]);
-                               x.appendChild(t);
-                               //li.appendChild(x);
-                               l.appendChild(x);
-                           }
-                           rf.appendChild(l)
-                           //y.appendChild(l);
-                       }
-                       
-                       else{
-                           var x = document.createElement(f[j].name);
-                           x.setAttribute('class',f[j].class);
-                           x.innerHTML = jobs[i][f[j].class];
-                           rf.appendChild(x)
-                       }
-                       
-                   }
+                   rf.setAttribute('class','job_desc');
+                   createElements(jobs[i], y, rf);
                    y.appendChild(rf);
                    m.appendChild(y);
-                   
                }
+
             }
-            
             
         })();
     }
+
+    //Get all Tags
+    function getAllTags(){
+        (async function getAll(){
+            const response = await fetch(url).then(response => response.json())
+            .then(data =>{
+                console.log("data tags")
+                getUniqueTags(data);
+            });
+        })()
+    }
+    getAllTags();
+
+
+    /** Get Unique Tags */
+    function getUniqueTags(data){
+        const arr = []
+        for(var i = 0; i < data.length; i++){
+
+            for(var l  in data[i].tags){
+                arr.push((data[i].tags[l]).toLowerCase());
+            }
+
+        } 
+
+        console.log(arr.length);
+        const e = document.getElementsByClassName('tags_section')[0];
+
+        const arr_new = Array.from(new Set(arr))
+        const div = document.createElement('div');
+       arr_new.forEach(tag =>{
+                //console.log(tag);
+                const btn = document.createElement('button');
+                const text = document.createTextNode(tag);
+                btn.addEventListener('click',function(){
+                    //event.preventDefault();
+                    //const j = getByTagsName(tag);
+                    //dataView(j);
+                    getByTagsName(tag);
+                })
+                btn.appendChild(text);
+                div.appendChild(btn);
+               
+        }) 
+        e.appendChild(div);
+    }
+
+    /**Getting Tags By name */
+    function  getByTagsName(tag){
+        var m = document.getElementsByClassName('container')[0];
+        m.innerHTML = ''
+        const arr = [];
+
+        (async function getTagsName(){
+            const response = await fetch(url).then(response => response.json())
+            .then(data =>{
+                for(let i = 0; i < data.length; i++){
+                  
+                    for(let j in data[i].tags){
+
+                        var y = document.createElement('div');
+                        y.setAttribute('class','jobs');
+                        const rf = document.createElement('div');
+                        rf.setAttribute('class','job_desc');
+
+                       if(data[i].tags[j] === tag){
+                           const rf = document.createElement('div');
+                           rf.setAttribute('class','job_desc');
+                           createElements(data[i], y, rf);
+                           y.appendChild(rf);
+                           m.appendChild(y);
+                       }
+                    }
+                }
+            })
+        })();
+    }
+
+
+    window.addEventListener('DOMContentLoaded',function(){
+        listJobs();
+    })
 
 
 }
 
 
-window.addEventListener('DOMContentLoaded',function(){
-    const m = document.createElement('main');
-    m.setAttribute('class', 'container');
-    document.body.appendChild(m);
-    App();
-})
+
+
+
+
+
+App();
+
 
